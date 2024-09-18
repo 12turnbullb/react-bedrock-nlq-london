e77b625532ac4658409c809bce8ea38ed40e8d84
Ben Turnbull <benbull@amazon.com>
2024-09-18 15:29:14 -0400

    initial commit

--- src/Agent.js
+++ src/Agent.js
@@ -1,52 +1,47 @@
-import { Link } from 'react-router-dom';
-import React, { useState, useEffect, useRef } from 'react';
-import axios from 'axios';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import './Chatbot_Agent.css'; // Import the external CSS file
-import loadingGif from './loading-gif.gif';
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import "./Chatbot_Agent.css"; // Import the external CSS file
+import loadingGif from "./loading-gif.gif";
 
 const Agent = ({ generated_uuid }) => {
   const [messages, setMessages] = useState([]);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [isLoading, setIsLoading] = useState(false); // State to handle loading
   const [expandedIndexes, setExpandedIndexes] = useState([]); // Track expanded messages
   const chatHistoryRef = useRef(null); // Reference to the chat history container
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (input.trim() === '') return;
+    if (input.trim() === "") return;
 
-    let agentId = 'CZ5AECXEPN';
-    let aliasId = "9FBV3HE1TE"
-
-    let encodedAgentId = encodeURIComponent(`${agentId}`);
-    let encodedAliasId = encodeURIComponent(`${aliasId}`);
     let encodedPrompt = encodeURIComponent(`${input}`);
 
     // Add user message to chat history
-    const newMessages = [...messages, { sender: 'user', text: input }];
+    const newMessages = [...messages, { sender: "user", text: input }];
     setMessages(newMessages);
 
     // Disable input and set loading state
     setIsLoading(true);
-    setInput('');
+    setInput("");
 
     try {
-      // Perform a GET request with parameters in the URL
-      const response = await axios.get(`https://gjft5j1cp8.execute-api.us-east-1.amazonaws.com/api/input/${encodedPrompt}/agentId/${encodedAgentId}/aliasId/${encodedAliasId}/id/${generated_uuid}`);
-      
+      const response = await axios.get(
+        `https://q47o63pkjb.execute-api.us-east-1.amazonaws.com/prod/input/${encodedPrompt}/id/${generated_uuid}`
+      );
+
       // Add bot response and SQL query to chat history
       const botMessage = {
-        sender: 'bot',
+        sender: "bot",
         text: response.data.answer, // Adjust based on the actual response structure
-        sql: response.data.sql_query
+        sql: response.data.sql_query,
       };
       setMessages((prevMessages) => [...prevMessages, botMessage]);
     } catch (error) {
-      console.error('Error fetching bot response:', error);
+      console.error("Error fetching bot response:", error);
     } finally {
-      // Set loading to false when the API calls is finished 
+      // Set loading to false when the API calls is finished
       setIsLoading(false);
     }
   };
@@ -59,83 +54,127 @@ const Agent = ({ generated_uuid }) => {
   }, [messages]);
 
   const toggleExpand = (index) => {
-    setExpandedIndexes((prev) => 
-      prev.includes(index) ? prev.filter(i => i !== index) : [...prev, index]
+    setExpandedIndexes((prev) =>
+      prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index]
     );
   };
-  
+
   return (
     <div>
-    <div className="button-group">
-      <Link to="/">
-        <button className="styled-button">Back to Home</button>
-      </Link>
-      <br />
-      <button className="styled-button" onClick={() => window.location.reload()}>Refresh</button>
-    </div>
+      <header className="chatbot-header">
+        <h1>Generative AI Forum - London, UK</h1>
+      </header>
 
-      <div className="chatbot-overlay">
-        {/* Title bar */}
-        <div className="chatbot-title">
-          PLTW Natural Language Query
+      <div className="container">
+        <div className="left-section">
+          <section className="about-section">
+            <h1 className="about-header">About this Demo</h1>
+            <p className="description">
+              We use Amazon Bedrock to generate SQL queries from natural
+              language and return the results conversationally. You can ask
+              questions about FIFA world cups from 1930-2014. The structured
+              data in our sample ends at 2014.
+            </p>
+            <h2 className="about-header">Dataset Descriptions</h2>
+            <p className="description">
+              This application utilizes several datasets to provide insights and
+              statistics about the FIFA World Cup:
+            </p>
+            <ul className="about-dataset-list">
+              <li>
+                <strong>World Cup Dataset:</strong> Tracks the winners of each
+                tournament along with the host countries and final match
+                results.
+              </li>
+              <li>
+                <strong>World Cup Matches Dataset:</strong> Contains detailed
+                match information including dates, venues, teams, scores, and
+                match events.
+              </li>
+              <li>
+                <strong>Players Dataset:</strong> Includes performance metrics
+                for players such as goals, assists, and minutes played.
+              </li>
+            </ul>
+          </section>
         </div>
-
-        <div className="chat-container">
-          <div className="chat-history" ref={chatHistoryRef}>
-            {messages.map((message, index) => (
-              <div key={index} className={`message ${message.sender}`}>
-                {message.sender === 'bot' ? (
-                  <div className="bot-message">
-                    <img
-                      src="./images/pltw_logo.png"
-                      alt="Bot Avatar"
-                      className="avatar"
-                    />
-                    <div className="text-bubble bot">
-                      <div className="markdown-content">
-                        <ReactMarkdown remarkPlugins={[remarkGfm]}>{message.text}</ReactMarkdown>
-                      </div>
-                      {message.sql && (
-                        <div className="expandable-section">
-                          <button className="expand-button" onClick={() => toggleExpand(index)}>
-                            {expandedIndexes.includes(index) ? 'Hide SQL' : 'Show SQL'}
-                          </button>
-                          {expandedIndexes.includes(index) && (
-                            <div className="sql-query">
-                              <div>{message.sql}</div>
+        <div className="right-section">
+          <div className="chatbot-overlay">
+            <div className="chatbot-title">
+              Natural Language Query with Amazon Bedrock
+            </div>
+            <div className="chat-container">
+              <div className="chat-history" ref={chatHistoryRef}>
+                {messages.map((message, index) => (
+                  <div key={index} className={`message ${message.sender}`}>
+                    {message.sender === "bot" ? (
+                      <div className="bot-message">
+                        <img
+                          src="./images/ball-avatar.png"
+                          alt="Bot Avatar"
+                          className="avatar"
+                        />
+                        <div className="text-bubble bot">
+                          <div className="markdown-content">
+                            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                              {message.text}
+                            </ReactMarkdown>
+                          </div>
+                          {message.sql && (
+                            <div className="expandable-section">
+                              <button
+                                className="expand-button"
+                                onClick={() => toggleExpand(index)}
+                              >
+                                {expandedIndexes.includes(index)
+                                  ? "Hide SQL"
+                                  : "Show SQL"}
+                              </button>
+                              {expandedIndexes.includes(index) && (
+                                <div className="sql-query">
+                                  <div>{message.sql}</div>
+                                </div>
+                              )}
                             </div>
                           )}
                         </div>
-                      )}
-                    </div>
+                      </div>
+                    ) : (
+                      <div className="user-message">
+                        <div className="text-bubble user">{message.text}</div>
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className="form-container">
+              <form onSubmit={handleSubmit} className="form">
+                <input
+                  type="text"
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                  placeholder="Type a message..."
+                  className="input"
+                  disabled={isLoading} // Disable input during loading
+                />
+                {isLoading ? (
+                  <div className="button-loading">
+                    <img
+                      src={loadingGif}
+                      alt="Loading"
+                      className="loading-gif"
+                    />
                   </div>
                 ) : (
-                  <div className="user-message">
-                    <div className="text-bubble user">{message.text}</div>
-                  </div>
+                  <button type="submit" className="button">
+                    Send
+                  </button>
                 )}
-              </div>
-            ))}
+              </form>
+            </div>
           </div>
-          <form onSubmit={handleSubmit} className="form">
-            <input
-              type="text"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              placeholder="Type a message..."
-              className="input"
-              disabled={isLoading} // Disable input during loading
-            />
-            {isLoading ? (
-              <div className="button-loading">
-                <img src={loadingGif} alt="Loading" className="loading-gif" />
-              </div>
-            ) : (
-              <button type="submit" className="button">
-                Send
-              </button>
-            )}
-          </form>
         </div>
       </div>
     </div>
--- src/App.js
+++ src/App.js
@@ -1,88 +1,16 @@
-import React, {useState, useEffect, useRef} from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Chatbot from './chatbot_markdown';
-import { ResponseArrayProvider } from './ResponseArrayContext';
-import { PromptArrayProvider } from './PromptArrayContext';
-import BackgroundComponent from './BackgroundComponent';
-import Agent from './Agent'
-import useLocalStorage from './useLocalStorage';
-import pltw from './customer_images/update_pltw.png';
+import React, { useState, useEffect, useRef } from "react";
+import Agent from "./Agent";
 
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
+
+const generated_uuid = uuidv4();
 
 const App = () => {
-    let bogus = pltw;
-    const isInitialMount = useRef(true);
-    const [inputIndexName, setIndexInputValue] = useState("abinbev_foundation-vector-embeddings");
-    const [savedIndex, setSavedIndex] = useLocalStorage('savedIndex', inputIndexName);
-    const [backgroundImage, setBackgroundImage] = useState(inputIndexName.replace("-vector-embeddings", "")); //Default background image
-    const generated_uuid = uuidv4();
-    //alert("cuustomer name: " + inputIndexName.replace("-vector-embeddings", ""));
-    const getSavedIndex = () => {
-        return savedIndex;
-      };
-    const changeBackground = (newImage) => {
-        setBackgroundImage(getImageReference(newImage));
-    }
-    useEffect(() => {
-        if (getSavedIndex()){
-            //alert("got saved index: " + getSavedIndex());
-            setIndexInputValue(getSavedIndex());
-            setBackgroundImage(getImageReference(getSavedIndex().replace("-vector-embeddings", "")));
-          }
-        //alert("running App update");
-    }, []);
-   return (
-       <Router>
-        <Routes>
-        <Route path = "/" element={
-            <div className="App" style={{ backgroundImage: backgroundImage}}>
-            <div className="button-group">
-              <Link to="/nlq">
-                <button className="styled-button">Go to NLQ</button>
-              </Link>
-              <br />
-              <button className="styled-button" onClick={() => window.location.reload()}>Refresh</button>
-            </div>
-            <BackgroundComponent imageUrl={backgroundImage} />
-            
-                <ResponseArrayProvider>
-                    <PromptArrayProvider>
-                    <Chatbot changeBackground={changeBackground} generated_uuid={generated_uuid} />
-                    </PromptArrayProvider>
-                </ResponseArrayProvider>    
-            </div>
-        } />
-        <Route path = "/nlq" element = {<Agent generated_uuid={generated_uuid} />} />
-       </Routes>
-        </Router>
-    );
-}
-
-function getImageReference(imageName) {
-    if (imageName === "pltw") {
-        return pltw;
-    }
-    else {
-        return pltw;
-    }
-}
+  return (
+    <div>
+      <Agent generated_uuid={generated_uuid} />
+    </div>
+  );
+};
 
 export default App;
-
-//const App = () => {
-//    return (
-//        <div className="App">
-//            
-//            <Chatbot />
-//        </div>
-//    );
-//}
-//return (
-//       
-//    <ArrayProvider>
-//        <ChildComponent />
-//    </ArrayProvider>
-// );
-
-//<BackgroundComponent imageUrl={backgroundImage} />
